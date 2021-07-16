e1c04b1ad3ababad55bac98079d976dd5329edbf
hanielchang <haniellloyd@gmail.com>
2021-07-15 20:19:02 -0700

    added random attack order

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,45 +27,54 @@ var fightOrSkip = function() {
   
   var fight = function(enemy) {
     console.log(enemy);
+    var isPlayerTurn = true;
+    if (Math.random() > 0.5) {
+      isPlayerTurn = false;
+    } 
+
     var choseSkip = fightOrSkip();
     while (playerInfo.health > 0 && enemy.health > 0)  {
+        if (isPlayerTurn) {
+          if (choseSkip) {
+              // if true, leave fight by breaking loop
+              break;
+          }
 
-        if (choseSkip) {
-            // if true, leave fight by breaking loop
-            break;
+          // generate random damage value based on player's attack power
+          var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+          enemy.health = Math.max(0, enemy.health - damage);
+  
+          console.log(
+          playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
+          );
+          // check enemy's health
+          if (enemy.health <= 0) {
+          window.alert(enemy.name + " has died!");
+          break;
+          } else {
+          window.alert(enemy.name + " still has " + enemy.health + " health left.");
+          }
+
         }
-
-        // generate random damage value based on player's attack power
-        var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-        enemy.health = Math.max(0, enemy.health - damage);
-
-        console.log(
-        playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
-        );
-    
-        // check enemy's health
-        if (enemy.health <= 0) {
-        window.alert(enemy.name + " has died!");
-        break;
-        } else {
-        window.alert(enemy.name + " still has " + enemy.health + " health left.");
+        else {
+          // generate random damage value based on enemy Attack power
+          var damage = randomNumber(enemy.attack - 3, enemy.attack);
+          playerInfo.health = Math.max(0, playerInfo.health - damage);
+  
+          console.log(
+          enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
+          );
+      
+          // check player's health
+          if (playerInfo.health <= 0) {
+          window.alert(playerInfo.name + " has died!");
+          break;
+          } else {
+          window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+          }        
         }
-    
-        // generate random damage value based on enemy Attack power
-        var damage = randomNumber(enemy.attack - 3, enemy.attack);
-        playerInfo.health = Math.max(0, playerInfo.health - damage);
-
-        console.log(
-        enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
-        );
-    
-        // check player's health
-        if (playerInfo.health <= 0) {
-        window.alert(playerInfo.name + " has died!");
-        break;
-        } else {
-        window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
-        }        
+    // switch turn order for next round
+    isPlayerTurn = !isPlayerTurn;  
     }
 };
 
